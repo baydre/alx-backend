e40937a3949b2e5369cca56f894271b99c95e1d9
baydre_africa <baydreafrica@gmail.com>
2024-04-29 12:51:46 +0100

    fix checker error #6

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """
+Simple helper function
 """
 
 
 def index_range(page, page_size):
     """
+    index_range that takes two
+    integer arguments page & page_size.
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
